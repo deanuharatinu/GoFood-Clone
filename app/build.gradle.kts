44c609f7159cb04a10859856eb04681d7415f5b1
deanuharatinu <deanu.alt@gmail.com>
2023-09-15 13:51:25 +0700

    add compose navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,8 @@ dependencies {
   implementation(libs.androidx.compose.ui.graphics)
   implementation(libs.androidx.compose.ui.tooling.preview)
   implementation(libs.androidx.compose.material3)
+  implementation(libs.androidx.navigation.compose)
+  implementation(libs.androidx.navigation.runtime.ktx)
   androidTestImplementation(composeBom)
   androidTestImplementation(libs.androidx.compose.ui.test.junit)
   androidTestImplementation(libs.androidx.compose.ui.tooling)
