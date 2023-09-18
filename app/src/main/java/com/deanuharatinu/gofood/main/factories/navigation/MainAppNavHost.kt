b0d28e4133aafa1eb9bd96161d64bd0d166a49ad
deanuharatinu <deanu.alt@gmail.com>
2023-09-18 16:29:53 +0700

    MainAppNavHost: add nested graph for 2 features

--- app/src/main/java/com/deanuharatinu/gofood/main/factories/navigation/MainAppNavHost.kt
+++ app/src/main/java/com/deanuharatinu/gofood/main/factories/navigation/MainAppNavHost.kt
@@ -4,38 +4,31 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.deanuharatinu.gofood.feature.login.ui.LoginScreen
-import com.deanuharatinu.gofood.feature.register.ui.InputAddressScreen
-import com.deanuharatinu.gofood.feature.register.ui.RegisterScreen
+import com.deanuharatinu.gofood.feature.login.ui.navigation.LOGIN_GRAPH_ROUTE
+import com.deanuharatinu.gofood.feature.login.ui.navigation.loginGraphRoute
+import com.deanuharatinu.gofood.feature.register.ui.navigation.REGISTER_GRAPH_ROUTE
+import com.deanuharatinu.gofood.feature.register.ui.navigation.registerGraphRoute
 
 @Composable
 fun MainAppNavHost(
   modifier: Modifier = Modifier,
   navHostController: NavHostController = rememberNavController(),
-  startDestination: String = "login",
+  startDestination: String = LOGIN_GRAPH_ROUTE,
 ) {
   NavHost(
     modifier = modifier,
     navController = navHostController,
     startDestination = startDestination,
   ) {
-    composable("login") {
-      LoginScreen(
-        onRegisterClick = { navHostController.navigate("register") },
-      )
-    }
-    composable("register") {
-      RegisterScreen(
-        onNavClick = { navHostController.popBackStack() },
-        onButtonClick = {navHostController.navigate("register/address") },
-      )
-    }
-    composable("register/address") {
-      InputAddressScreen(
-        onNavClick = { navHostController.popBackStack() },
-      )
-    }
+    loginGraphRoute(
+      onRegisterClick = { navHostController.navigate(REGISTER_GRAPH_ROUTE) }
+    )
+    registerGraphRoute(
+      onNavClick = { navHostController.popBackStack() },
+      onButtonClick = { destination ->
+        navHostController.navigate(destination)
+      },
+    )
   }
 }
