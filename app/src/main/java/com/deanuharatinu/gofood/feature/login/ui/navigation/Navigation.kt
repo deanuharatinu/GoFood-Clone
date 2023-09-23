ff066a349fae81f74ccf8bffe410bc03042bfbc7
deanuharatinu <deanu.alt@gmail.com>
2023-09-24 01:13:06 +0700

    add ViewModel on login Navigation.kt

--- app/src/main/java/com/deanuharatinu/gofood/feature/login/ui/navigation/Navigation.kt
+++ app/src/main/java/com/deanuharatinu/gofood/feature/login/ui/navigation/Navigation.kt
@@ -1,9 +1,15 @@
 package com.deanuharatinu.gofood.feature.login.ui.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import com.deanuharatinu.gofood.feature.login.presentation.LoginViewModel
 import com.deanuharatinu.gofood.feature.login.ui.LoginScreen
+import com.deanuharatinu.gofood.main.factories.ViewModelFactory
 
 const val LOGIN_GRAPH_ROUTE = "login-graph"
 const val LOGIN = "login"
@@ -16,9 +22,25 @@ fun NavGraphBuilder.loginGraphRoute(
     startDestination = LOGIN,
   ) {
     composable(LOGIN) {
-      LoginScreen(
+      LoginScreenRoot(
+        viewModel = viewModel(factory = ViewModelFactory.provideLoginViewModel()),
         onRegisterClick = onRegisterClick,
       )
     }
+  }
+}
+
+@Composable
+fun LoginScreenRoot(
+  viewModel: LoginViewModel,
+  onRegisterClick: () -> Unit,
+) {
+  val loginUiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+  LoginScreen(
+    loginUiState = loginUiState,
+    onRegisterClick = onRegisterClick
+  ) { email, password ->
+    viewModel.login(email, password)
   }
 }
