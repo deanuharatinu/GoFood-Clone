6173b38f7a58391c87c3aaa01ce0f67bad2c47ca
deanuharatinu <deanu.alt@gmail.com>
2023-09-24 01:01:06 +0700

    add Const

---
+++ app/src/main/java/com/deanuharatinu/gofood/core/util/Const.kt
@@ -0,0 +1,7 @@
+package com.deanuharatinu.gofood.core.util
+
+object Const {
+  const val BASE_URL = "http://foodmarket-api.aryaaditiya.com/"
+  const val LOGIN = "api/login"
+  const val REGISTER = "api/register"
+}
