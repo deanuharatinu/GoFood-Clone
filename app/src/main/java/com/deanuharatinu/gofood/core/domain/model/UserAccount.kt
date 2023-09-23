8cd20c9400d7801704358a655acdbad7ecdfe91b
deanuharatinu <deanu.alt@gmail.com>
2023-09-24 01:08:10 +0700

    add UserAccount

--- app/src/main/java/com/deanuharatinu/gofood/core/domain/model/UserAccount.kt
+++ app/src/main/java/com/deanuharatinu/gofood/core/domain/model/UserAccount.kt
@@ -1,3 +1,26 @@
 package com.deanuharatinu.gofood.core.domain.model
 
+data class UserAccount(
+  val accessToken: String,
+  val tokenType: String,
+  val user: User,
+) {
+//  fun isEmailValid(email: String): Boolean {
+//    return email.matches(EMAIL_REGEX.toRegex())
+//  }
+}
 
+data class User(
+  val id: Int,
+  val name: String,
+  val email: String,
+  val roles: String,
+  val address: String,
+  val houseNumber: String,
+  val phoneNumber: String,
+  val city: String,
+  val createdAt: Long,
+  val updatedAt: Long,
+  val profilePhotoPath: String,
+  val profilePhotoUrl: String,
+)
