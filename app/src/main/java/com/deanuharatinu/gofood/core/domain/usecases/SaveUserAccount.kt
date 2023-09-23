a36cc8d13d943912151816106c7b769eb781b55d
deanuharatinu <deanu.alt@gmail.com>
2023-09-24 01:08:58 +0700

    update param on SaveUserAccount

--- app/src/main/java/com/deanuharatinu/gofood/core/domain/usecases/SaveUserAccount.kt
+++ app/src/main/java/com/deanuharatinu/gofood/core/domain/usecases/SaveUserAccount.kt
@@ -1,5 +1,7 @@
 package com.deanuharatinu.gofood.core.domain.usecases
 
+import com.deanuharatinu.gofood.core.domain.model.UserAccount
+
 fun interface SaveUserAccount {
-  suspend fun saveUserAccount(): Boolean
+  suspend fun saveUserAccount(userAccount: UserAccount): Boolean
 }
