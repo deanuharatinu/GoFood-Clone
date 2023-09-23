767feb1b98939c021609eff3f3e062ce4b856ef5
deanuharatinu <deanu.alt@gmail.com>
2023-09-24 01:09:28 +0700

    add LoginViewModel

---
+++ app/src/main/java/com/deanuharatinu/gofood/feature/login/presentation/LoginViewModel.kt
@@ -0,0 +1,45 @@
+package com.deanuharatinu.gofood.feature.login.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.deanuharatinu.gofood.core.domain.Resource
+import com.deanuharatinu.gofood.feature.login.domain.usecases.LoginAccount
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+data class LoginViewModelState(
+  val isLoading: Boolean = false,
+  val failed: String = ""
+)
+
+class LoginViewModel constructor(
+  private val loginAccount: LoginAccount
+) : ViewModel() {
+  private val _uiState = MutableStateFlow(LoginViewModelState())
+  val uiState = _uiState.asStateFlow()
+
+  fun login(email: String, password: String) {
+    viewModelScope.launch(Dispatchers.IO) {
+      _uiState.value = _uiState.value.copy(isLoading = true)
+
+      val result = loginAccount.loginAccount(email, password)
+      withContext(Dispatchers.Main) {
+        when (result) {
+          is Resource.Success -> {
+            _uiState.value = _uiState.value.copy(isLoading = false)
+          }
+
+          is Resource.Error -> {
+            _uiState.value = _uiState.value.copy(
+              isLoading = false,
+              failed = result.message
+            )
+          }
+        }
+      }
+    }
+  }
+}
