7ef5965364d6fe804a3cf914384fd04cf66f6d45
deanuharatinu <deanu.alt@gmail.com>
2023-09-24 16:03:50 +0700

    update parameter for InputAddressScreen

--- app/src/main/java/com/deanuharatinu/gofood/feature/register/ui/InputAddressScreen.kt
+++ app/src/main/java/com/deanuharatinu/gofood/feature/register/ui/InputAddressScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -15,6 +17,8 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.deanuharatinu.gofood.R
+import com.deanuharatinu.gofood.feature.register.presentation.RegisterViewModelState
+import com.deanuharatinu.gofood.feature.register.ui.model.RegisterDataState
 import com.deanuharatinu.gofood.ui.component.AppBarComponent
 import com.deanuharatinu.gofood.ui.component.ButtonComponent
 import com.deanuharatinu.gofood.ui.component.DropDownComponent
@@ -24,9 +28,13 @@ import com.deanuharatinu.gofood.ui.theme.GoFoodTheme
 @Composable
 fun InputAddressScreen(
   modifier: Modifier = Modifier,
+  registerUiState: RegisterViewModelState = RegisterViewModelState(),
   dropDownList: List<String>,
   onNavClick: () -> Unit,
+  onRegisterClick: (RegisterDataState) -> Unit,
 ) {
+  val registerDataState = remember { mutableStateOf(registerUiState.registerDataState) }
+
   Scaffold(
     modifier = modifier,
     topBar = {
@@ -54,7 +62,12 @@ fun InputAddressScreen(
             keyboardType = KeyboardType.Number,
             imeAction = ImeAction.Next
           ),
-          onValueChange = {}
+          onValueChange = {
+            registerDataState.value = registerDataState.value.copy(
+              phoneNumber = it
+            )
+          },
+          value = registerDataState.value.phoneNumber,
         )
         Spacer(modifier = Modifier.padding(vertical = 8.dp))
         TextFieldComponent(
@@ -64,7 +77,12 @@ fun InputAddressScreen(
             keyboardType = KeyboardType.Text,
             imeAction = ImeAction.Next
           ),
-          onValueChange = {}
+          onValueChange = {
+            registerDataState.value = registerDataState.value.copy(
+              address = it
+            )
+          },
+          value = registerDataState.value.address,
         )
         Spacer(modifier = Modifier.padding(vertical = 8.dp))
         TextFieldComponent(
@@ -74,13 +92,24 @@ fun InputAddressScreen(
             keyboardType = KeyboardType.Number,
             imeAction = ImeAction.Done
           ),
-          onValueChange = {}
+          onValueChange = {
+            registerDataState.value = registerDataState.value.copy(
+              houseNumber = it
+            )
+          },
+          value = registerDataState.value.houseNumber,
         )
         Spacer(modifier = Modifier.padding(vertical = 8.dp))
         DropDownComponent(
           label = stringResource(id = R.string.register_city),
           placeholder = stringResource(id = R.string.register_city_placeholder),
           dropDownList = dropDownList,
+          selectedItem = {
+            registerDataState.value = registerDataState.value.copy(
+              city = it
+            )
+          },
+          value = registerDataState.value.city,
         )
       }
 
@@ -88,8 +117,8 @@ fun InputAddressScreen(
 
       ButtonComponent(
         modifier = Modifier.padding(16.dp),
-        buttonText = stringResource(id = R.string.register_continue),
-        onClick = {}
+        buttonText = stringResource(id = R.string.register_register_now),
+        onClick = { onRegisterClick.invoke(registerDataState.value) }
       )
     }
   }
@@ -101,7 +130,8 @@ private fun InputAddressScreenPreview() {
   GoFoodTheme {
     InputAddressScreen(
       dropDownList = listOf("Jakarta", "Bandung", "Semarang", "Surabaya", "Malang"),
-      onNavClick = {}
+      onNavClick = {},
+      onRegisterClick = {},
     )
   }
 }
