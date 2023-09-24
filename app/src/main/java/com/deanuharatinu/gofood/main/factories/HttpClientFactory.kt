338b891fb881e2be754c38b2eb6855086eb8ea8e
deanuharatinu <deanu.alt@gmail.com>
2023-09-24 16:02:57 +0700

    add createRegisterHttpClient

--- app/src/main/java/com/deanuharatinu/gofood/main/factories/HttpClientFactory.kt
+++ app/src/main/java/com/deanuharatinu/gofood/main/factories/HttpClientFactory.kt
@@ -2,12 +2,19 @@ package com.deanuharatinu.gofood.main.factories
 
 import com.deanuharatinu.gofood.feature.login.http.LoginHttpClient
 import com.deanuharatinu.gofood.feature.login.http.LoginHttpClientImpl
+import com.deanuharatinu.gofood.feature.register.http.RegisterHttpClient
+import com.deanuharatinu.gofood.feature.register.http.RegisterHttpClientImpl
 
 class HttpClientFactory {
   companion object {
     fun createLoginHttpClient(): LoginHttpClient {
       val loginService = HttpServiceFactory.createLoginService()
       return LoginHttpClientImpl(loginService)
+    }
+
+    fun createRegisterHttpClient(): RegisterHttpClient {
+      val registerService = HttpServiceFactory.createRegisterService()
+      return RegisterHttpClientImpl(registerService)
     }
   }
 }
