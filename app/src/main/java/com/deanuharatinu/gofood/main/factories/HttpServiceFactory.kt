a9e17bf139807f799223b1d389386834dd8362ac
deanuharatinu <deanu.alt@gmail.com>
2023-09-24 16:02:48 +0700

    add createRegisterService

--- app/src/main/java/com/deanuharatinu/gofood/main/factories/HttpServiceFactory.kt
+++ app/src/main/java/com/deanuharatinu/gofood/main/factories/HttpServiceFactory.kt
@@ -2,6 +2,7 @@ package com.deanuharatinu.gofood.main.factories
 
 import com.deanuharatinu.gofood.core.frameworks.HttpFactory
 import com.deanuharatinu.gofood.feature.login.http.LoginService
+import com.deanuharatinu.gofood.feature.register.http.RegisterService
 
 class HttpServiceFactory {
   companion object {
@@ -11,6 +12,14 @@ class HttpServiceFactory {
       val okHttpClient = HttpFactory.createOkhttpClient(loggingInterceptor)
       val retrofit = HttpFactory.createRetrofit(moshi, okHttpClient)
       return retrofit.create(LoginService::class.java)
+    }
+
+    fun createRegisterService(): RegisterService {
+      val moshi = HttpFactory.createMoshi()
+      val loggingInterceptor = HttpFactory.createLoggingInterceptor()
+      val okHttpClient = HttpFactory.createOkhttpClient(loggingInterceptor)
+      val retrofit = HttpFactory.createRetrofit(moshi, okHttpClient)
+      return retrofit.create(RegisterService::class.java)
     }
   }
 }
