c9a26c2cd1f08b6cfcbe0449a65927ce1841aafd
deanuharatinu <deanu.alt@gmail.com>
2023-09-24 12:19:38 +0700

    add CacheFactory

---
+++ app/src/main/java/com/deanuharatinu/gofood/core/frameworks/CacheFactory.kt
@@ -0,0 +1,12 @@
+package com.deanuharatinu.gofood.core.frameworks
+
+import android.content.Context
+import android.content.SharedPreferences
+
+object CacheFactory {
+  private const val SHARED_PREFERENCES_ID = "go-food-clone"
+
+  fun createSharedPreference(context: Context): SharedPreferences {
+    return context.getSharedPreferences(SHARED_PREFERENCES_ID, Context.MODE_PRIVATE)
+  }
+}
